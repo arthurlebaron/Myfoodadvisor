b1e2e2e5afa585493ef92fef8b4ce71c52a2e06d
a.lebaron.15 <arthurlebaron@gmail.com>
2017-11-06 14:59:07 -0500

    connexion fb

--- app/src/main/java/com/myfoodadvisor/myfoodadvisor/Myfoodadvisor.java
+++ app/src/main/java/com/myfoodadvisor/myfoodadvisor/Myfoodadvisor.java
@@ -1,13 +1,56 @@
 package com.myfoodadvisor.myfoodadvisor;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class Myfoodadvisor extends AppCompatActivity {
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
 
+public class Myfoodadvisor extends AppCompatActivity {
+    CallbackManager callbackManager;
+    LoginButton loginButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_myfoodadvisor);
+
+        callbackManager = CallbackManager.Factory.create();
+
+        loginButton = (LoginButton) findViewById(R.id.login_button);
+
+        loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                // Connection réussi
+                String id = loginResult.getAccessToken().getUserId();
+                Profile profile = Profile.getCurrentProfile();
+
+                Intent i = new Intent(Myfoodadvisor.this, mon_profil.class);
+                startActivity(i);
+
+            }
+            @Override
+            public void onCancel() {
+                // Connection annulée
+            }
+            @Override
+            public void onError(FacebookException e) {
+                // Erreur de connexion
+            }
+        });
     }
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
+
+
 }
+
---
+++ app/src/main/java/com/myfoodadvisor/myfoodadvisor/mon_profil.java
@@ -0,0 +1,20 @@
+package com.myfoodadvisor.myfoodadvisor;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.os.Bundle;
+
+import com.facebook.FacebookSdk;
+
+/**
+ * Created by Utilisateur on 06/11/2017.
+ */
+
+public class mon_profil extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mon_profil);
+
+    }
+}
