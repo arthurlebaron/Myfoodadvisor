e7497fc3a78b19034049232278912f3e2b05b42d
a.lebaron.15 <arthurlebaron@gmail.com>
2017-11-14 15:26:48 -0500

    Auto stash before merge of "master" and "origin/master"

--- app/src/main/java/com/myfoodadvisor/myfoodadvisor/modification/modificationregime.java
+++ app/src/main/java/com/myfoodadvisor/myfoodadvisor/modification/modificationregime.java
@@ -70,7 +70,7 @@ public class modificationregime extends Activity implements View.OnClickListener
     @Override
     public void onClick(View view) {
         String username = prefs.getString("Pseudo/email", null);
-        if (Regime.getSelectedItem().toString().length() != 0){
+        if (Regime.getSelectedItem().toString() != "Faire un choix"){
             mRef.child("users").child(username).addListenerForSingleValueEvent(new ValueEventListener() {
                 @Override
                 public void onDataChange(DataSnapshot dataSnapshot) {
